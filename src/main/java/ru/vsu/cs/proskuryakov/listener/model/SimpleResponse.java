f8dada9986de3363f2f6cdcf15e76a6568c8d35d
eproskuryakov <proskur.egor@yandex.ru>
2023-06-05 16:21:39 +0300

    lab2

--- src/main/java/ru/vsu/cs/proskuryakov/listener/model/SimpleResponse.java
+++ src/main/java/ru/vsu/cs/proskuryakov/listener/model/SimpleResponse.java
@@ -1,8 +1,10 @@
 package ru.vsu.cs.proskuryakov.listener.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class SimpleResponse {
     private int status;
     private String data;
